441ecd7f4359a75dbb671fb616f32ad151c7ee02
Kaloyan Nikolov <62281709+Knikolov-92@users.noreply.github.com>
2020-03-23 19:01:50 +0200

    Add files via upload
    Updated registration.feature, RegistrationSteps, RegistrationPage, added BasePage and browser-config.properties.

--- src/test/java/stepdefs/RegistrationSteps.java
+++ src/test/java/stepdefs/RegistrationSteps.java
@@ -2,95 +2,55 @@ package stepdefs;
 
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.*;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 import page_objects.RegistrationPage;
-import java.util.concurrent.TimeUnit;
-import static org.testng.Assert.*;
 
 public class RegistrationSteps {
 
-//==========================================general declarations==========================================\\
-    private RegistrationPage regPage = new RegistrationPage();
-    private static WebDriver driver;
-    private static final String MyAccountURL = "https://shop.demoqa.com/my-account/";
 
+//==========================================general declarations==========================================\\
+    RegistrationPage regPage = new RegistrationPage();
 //==========================================Test Preconditions==========================================\
 
     @Given("The user has navigated to My Account Page")
     public void theUserHasNavigatedToMyAccountPage() {
 
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(MyAccountURL);
-        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-
-        regPage = PageFactory.initElements(driver, RegistrationPage.class);
+        regPage.navigateToRegistrationPage();
       }
 
-    @Then("The user should see the correct page title")
-    public void theUserShouldSeeTheCorrectPageTitle() {
+    @Then("The user sees the correct MyAccount page title")
+    public void theUserSeesTheCorrectPageTitle() {
 
-        String expectedPageTitle = "My Account – ToolsQA Demo Site";
-
-        assertEquals(driver.getTitle(), expectedPageTitle);
+       regPage.checkMyAccountPageTitle();
     }
 //==========================================New Scenario==========================================\
     @When("The user scrolls down the page")
-    public void theUserScrollsDownThePage() throws InterruptedException {
+    public void theUserScrollsDownThePage() {
 
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        for (int i = 0; i < 4; i++) {
-            js.executeScript("window.scrollBy(0,100)");
-            Thread.sleep(500);
-        }
+        regPage.scrollWindow(regPage.registerButton);
     }
 //------------------------------------------------------------
     @Then("The user should see the Registration form")
     public void theUserShouldSeeTheRegistrationForm() {
 
-        WebElement[] elementsArray = {
-                regPage.registerForm,
-                regPage.regUsernameLabel,regPage.regUsernameField,
-                regPage.regEmailLabel,regPage.regEmailField,
-                regPage.regPasswordLabel,regPage.regPasswordField,
-                regPage.registerButton,
-        };
-//------------------------------------------------------------
-       for (int n = 0; n < elementsArray.length; n++) {
-
-           Assert.assertTrue(elementsArray[n].isDisplayed());
-       }
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        regPage.checkRegistrationForm();
     }
 //====================================================================================\
     @And("^The user should see the Register button$")
     public void theUserShouldSeeTheRegisterButton() {
 
         regPage.registerButtonShouldBeSeen();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
 //====================================================================================\
     @Given("User enters personal details:")
     public void userEntersPersonalDetailsInRegForm(DataTable table ) {
 
        regPage.userEntersPersonalDetailsInRegForm(table);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
 //------------------------------------------------------------
     @When("^The Register Button is clicked$")
     public void theRegisterButtonIsClicked() {
 
         regPage.registerButtonIsClicked();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 //====================================================================================\
     @Then("^The user should see the (.*) error$")
@@ -101,11 +61,6 @@ public class RegistrationSteps {
 //====================================================================================\
     @Then("^The user should see the (.*) password warning$")
     public void theUserShouldSeePasswordWarning(String warning) {
-
-        Actions actions = new Actions(driver);
-        actions.doubleClick(regPage.regPasswordField).perform();
-        actions.doubleClick(regPage.regUsernameField).perform();
-        actions.doubleClick(regPage.regPasswordField).perform();
 
         regPage.checkPasswordWarning(warning);
     }
@@ -123,10 +78,27 @@ public class RegistrationSteps {
 
     @But("^The browser should close$")
     public void browserShouldClose() {
-        driver.quit();
+
+       regPage.endBrowserSession();
     }
 
+    @But("^The window is closed$")
+    public void windowIsClosed() throws InterruptedException {
 
+        regPage.endWindowSession();
+    }
+
+    @Given("^Privacy policy link is clicked$")
+    public void privacyPolicyLinkIsClicked() {
+
+       regPage.clickPrivacyPolicyLink();
+    }
+
+    @Then("The user should see the privacy policy page in new tab")
+    public void theUserShouldSeeThePrivacyPolicyPageInNewTab() {
+
+        regPage.checkPrivacyPolicyPage();
+    }
 //====================================================================================\
 
 //====================================================================================\
